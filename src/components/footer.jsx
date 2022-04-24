dce015b1e860600fd7297dbcee540387e0df447a
moasko.dev <42101671+moasko@users.noreply.github.com>
2022-04-24 18:21:46 +0000

    hgq

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+function Footer(){
+    return(
+        <p>merci de votre fidelité !</p>
+    )
+}
+
+export default Footer
---
+++ src/components/hearder.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+
+function Hearder(){
+    return (
+        <div>
+            <a href="">Acceuil</a>
+        <a href="">A-Propos</a>
+        <a href="">Blog</a>
+        </div>
+        
+
+    )
+}
+
+export default Hearder
